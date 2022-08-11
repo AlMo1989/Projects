2d1b800f966d00e4f8f36b0d3255c9065c725aa3
Aleksandr <almoshnik89@gmail.com>
2022-08-11 19:45:30 +0300

    Tests

---
+++ L2_C/Test/Program.cs
@@ -0,0 +1,42 @@
+﻿/* int a = 1;
+int b = 2;
+bool result = a > b;
+
+Console.WriteLine(result); */
+
+/* for(int i = 1;i <= 30; i++)
+{
+  Console.Write(i);
+} */
+
+/* for(int i = 0;i <= 30; i+=10)
+{
+  Console.WriteLine(i);
+} */
+
+/* for(int i = - 123; i <= 1000; i+=10)
+{
+ if(i%45 == 0) {
+  Console.WriteLine(i);
+ }
+} */
+
+/* for(int i = 1000; i >= -132; i--)
+{
+  Console.WriteLine(i);
+} */
+
+void SayHello(int num)
+{
+if(num == 1)
+{ 
+Console.WriteLine("Hello!");
+}
+}
+
+/* int Sum(int a, int b)
+
+{
+ int result = a + b;
+ return result;
+} */

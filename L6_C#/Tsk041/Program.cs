eb1854860f8b354356ffea80d83e02e449bb9c21
Aleksandr <almoshnik89@gmail.com>
2022-08-24 15:47:05 +0300

    Homework_Lesson 6

---
+++ L6_C#/Tsk041/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write($"Enter numbers quantity (M): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arrayNums = new int[m];
+
+void InputNumbers(int m)
+{
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Enter {i+1} number: ");
+    arrayNums[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int GetCompared(int[] arrayNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < arrayNumbers.Length; i++)
+  {
+    if(arrayNumbers[i] > 0 ) count++; 
+  }
+  return count;
+}
+
+InputNumbers(m);
+Console.WriteLine($"Entered numbers greater than zero: {GetCompared(arrayNums)} ");
---
+++ L6_C#/Tsk043/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+double[,] coeff = new double[2, 2];
+double[] crossPoint = new double[2];
+
+void InputCoefficients()
+{
+  for (int i = 0; i < coeff.GetLength(0); i++)
+  {
+    Console.Write($"Enter coef`s of {i+1} equation (y = k * x + b):\n");
+    for (int j = 0; j < coeff.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Enter coef. k: ");
+      else Console.Write($"Enter coef. b: ");
+      coeff[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+double[] GetDecision(double[,] coeff)
+{
+  crossPoint[0] = (coeff[1,1] - coeff[0,1]) / (coeff[0,0] - coeff[1,0]);
+  crossPoint[1] = crossPoint[0] * coeff[0,0] + coeff[0,1];
+  return crossPoint;
+}
+
+void GetResponse(double[,] coeff)
+{
+  if (coeff[0,0] == coeff[1,0] && coeff[0,1] == coeff[1,1]) 
+  {
+    Console.WriteLine("Lines is match");
+  }
+  else if (coeff[0,0] == coeff[1,0] && coeff[0,1] != coeff[1,1]) 
+  {
+    Console.WriteLine("Lines is parallel");
+  }
+  else 
+  {
+    GetDecision(coeff);
+    Console.WriteLine($"Lines crossing point: ({crossPoint[0]}, {crossPoint[1]})\n");
+  }
+}
+
+InputCoefficients();
+GetResponse(coeff);

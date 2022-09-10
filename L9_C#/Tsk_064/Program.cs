a33508340e8a78b1779713040bc83084d8a083d7
Aleksandr <almoshnik89@gmail.com>
2022-09-11 01:07:07 +0300

    Lesson 9 homework

---
+++ L9_C#/Tsk_064/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+
+int M = AskNumber("M");
+int N = AskNumber("N");
+Console.WriteLine($"M = {M}; N = {N}. -> \"{PrintRange(M, N)}\"");
+
+string PrintRange(int M, int N) 
+{
+    if(N < M) return " ";
+    string part = N == M ? $"{M}" : $"{M}, ";
+    return part + PrintRange(M + 1, N);
+}
+
+int AskNumber(string name) 
+{
+    Console.Write($"Enter the number {name}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
